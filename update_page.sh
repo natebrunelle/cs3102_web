6b9b6d99ef6073e5ab854dcc4159d3b36e99e6a2
natebrunelle <njb2b@virginia.edu>
2018-11-20 17:33:23 -0500

    index update

--- update_page.sh
+++ update_page.sh
@@ -1,8 +1,6 @@
 python3 gen_hub.py
 git add lecture_list
 git add index.html
-git add slides/*.pptx
-git add slides/*.pdf
 git commit -m $0
 git push
 cat remote_update.sh | ssh archimedes.cs.virginia.edu
